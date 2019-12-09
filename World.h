603bd9b8f4aebc328a01dd547a134bb884eeaea4
DivineWalker <49419110+DivineWalker@users.noreply.github.com>
2019-12-09 15:29:18 -0500

    Create World.h

---
+++ World.h
@@ -0,0 +1,25 @@
+#ifndef WORLD_H
+#define WORLD_H
+#include <cstdlib>
+
+using namespace std; 
+
+class World
+{
+	
+	public: 
+		World() {};
+		
+		virtual void PrintGrid() = 0;
+		virtual void UpdateGrid() = 0;
+		
+	protected:
+		int day; 
+		char grid[10][10];
+		
+		virtual void initGrid() = 0;
+		virtual bool checkBounds(int,int) = 0;
+	
+};
+
+#endif /* WORLD_H */
